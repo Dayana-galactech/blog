cade4c1d4422c596fabf8dcb392bb96bb04fcc04
Dayana <dayana.azzam@galactech.io>
2022-06-30 12:02:36 +0300

    create category

--- controllers/Categories.php
+++ controllers/Categories.php
@@ -25,7 +25,7 @@ class Categories extends Controller
         ];
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $csrf = $_SESSION['csrf_token'];
+            $csrf = $_SESSION['csrf_category'];
             if (!empty($_POST['name'])) {
                 if (isset($_POST['csrf']) && hash_equals($csrf, $_POST['csrf'])) {
                     $name = htmlspecialchars($_POST['name']);
