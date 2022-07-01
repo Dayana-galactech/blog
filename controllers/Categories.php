d6bac239e210d911ae38544db84f7dd89f48aaf8
Dayana <dayana.azzam@galactech.io>
2022-07-01 14:45:02 +0300

    fixing sessions for categories

--- controllers/Categories.php
+++ controllers/Categories.php
@@ -25,9 +25,9 @@ class Categories extends Controller
         ];
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $csrf = $_SESSION['csrf_createCategory'];
+            $csrf = $_SESSION['token'];
             var_dump($csrf);
-            var_dump($_SESSION['csrf_createCategory']);
+            var_dump($_SESSION['token']);
             if (!empty($_POST['name'])) {
                 if (isset($_POST['csrf']) && hash_equals($csrf, $_POST['csrf'])) {
                     $name = htmlspecialchars($_POST['name']);
