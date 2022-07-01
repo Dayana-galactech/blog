6835e9ce66602f3ec2489ff60de47441324b735e
Dayana <dayana.azzam@galactech.io>
2022-07-01 11:54:23 +0300

    text box

--- views/createPost.php
+++ views/createPost.php
@@ -140,7 +140,7 @@ if (session_id() == '') {
         selector: '#body',
         plugins: [
           'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+          'lists','link','charmap','preview','anchor','searchreplace','visualblocks',
           'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
         ],
         toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
