df31253a1ace5a94bd487367242402a792921190
Dayana <dayana.azzam@galactech.io>
2022-07-01 14:58:19 +0300

    fixing sessions for categories

--- views/index.php
+++ views/index.php
@@ -22,7 +22,7 @@
     }
     $secret = "secretKey";
     $csrf = hash_hmac('SHA256', uniqid(microtime()), $secret);
-    $_SESSION['tokens'] = $csrf;
+    $_SESSION['token'] = $csrf;
     $csrf_comment = hash_hmac('SHA256', uniqid(microtime()), $secret);
     $_SESSION['csrf_comment'] = $csrf_comment;
     require_once('./controllers/get.php');
